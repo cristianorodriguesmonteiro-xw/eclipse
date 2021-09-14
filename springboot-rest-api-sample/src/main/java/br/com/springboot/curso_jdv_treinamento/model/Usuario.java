59ba7cb6b427c2ed6a1a438953476f57ce9ca0e2
cristianorodriguesmonteiro-xw <90638226+cristianorodriguesmonteiro-xw@users.noreply.github.com>
2021-09-14 16:47:17 -0300

    Adicionei arquivos via upload

---
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdv_treinamento/model/Usuario.java
@@ -0,0 +1,54 @@
+package br.com.springboot.curso_jdv_treinamento.model;
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+
+@Entity
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+	
+	private long id;
+	
+	private String nome;
+	
+	private int idade;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	
+
+}
